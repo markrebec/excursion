598afa80aceda2734efe0d36fcf0d975b37e02f1
Mark Rebec <mark@markrebec.com>
2013-06-19 12:50:36 -0700

    extremely hacky solution for rails3 - does not support :format (just strips it out completely)

--- lib/excursion/helpers/url_helper.rb
+++ lib/excursion/helpers/url_helper.rb
@@ -9,9 +9,14 @@ module Excursion
 
       def method_missing(meth, *args)
         if meth.to_s.match(/\A(#{routes.collect { |name,route| name }.join("|")})_(url|path)\Z/)
-          ActionDispatch::Routing::RouteSet::NamedRouteCollection::UrlHelper.create(routes.get($1.to_sym), @application.default_url_options).call(Rails.application.routes, args)
-          # Playing with getting Rails3 working
-          #ActionDispatch::Http::URL.url_for(@application.default_url_options.merge({path: replaced_path(routes.get($1.to_sym), args) }))
+          if Excursion.rails4?
+            ActionDispatch::Routing::RouteSet::NamedRouteCollection::UrlHelper.create(routes.get($1.to_sym), @application.default_url_options).call(Rails.application.routes, args)
+          elsif Excursion.rails3?
+            # Playing with getting Rails3 working
+            #mapping = ActionDispatch::Routing::Mapper::Mapping.new(routes, {}, routes.get($1.to_sym).path.spec.to_s.dup, {:controller => 'dummy', :action => 'dummy'})
+            #puts mapping.send(:instance_variable_get, :@path)
+            ActionDispatch::Http::URL.url_for(@application.default_url_options.merge({path: replaced_path(routes.get($1.to_sym), args)}))
+          end
         else
           super
         end
@@ -24,13 +29,16 @@ module Excursion
       end
 
       # Playing with getting Rails3 working
-      #def replaced_path(route, args)
-      #  path = route.path.spec.to_s.dup
-      #  route.required_parts.zip(args) do |part, arg|
-      #    path.gsub!(/(\*|:)#{part}/, Journey::Router::Utils.escape_fragment(arg.to_param))
-      #  end
-      #  path
-      #end
+      def replaced_path(route, args)
+        path = route.path.spec.to_s.dup # ActionDispatch::Routing::Mapper.normalize_path(route.path.spec.to_s.dup)
+
+        route.required_parts.zip(args) do |part, arg|
+          path.gsub!(/(\*|:)#{part}/, Journey::Router::Utils.escape_fragment(arg.to_param))
+        end
+
+        path.gsub!(/\(\.:format\)/, '') # This is really gross, and :format should actually be supported
+        path
+      end
     end
   end
 end
