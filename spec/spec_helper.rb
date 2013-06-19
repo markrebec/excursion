9d66042d25b745d00d430f66d8fb7e8b7e85e343
Mark Rebec <mark@markrebec.com>
2013-06-19 13:57:54 -0700

    using in-memory sqlite db for specs (still need the dummy app for url helper specs)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/core'
-#require 'rspec/rails'
-#require 'rspec/autorun'
+
+ActiveRecord::Base.establish_connection :adapter => "sqlite3",
+                                        :database => ":memory:"
+ActiveRecord::Migrator.migrate(File.join(File.dirname(__FILE__), "dummy/db/migrate"))
 
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
 
