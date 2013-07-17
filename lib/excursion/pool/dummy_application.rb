0b303d1c255f088ee380514a96e89ba7e4877ea8
Mark Rebec <mark@markrebec.com>
2013-07-16 20:06:48 -0700

    adding [] and has_key? support methods to the dummy application

--- lib/excursion/pool/dummy_application.rb
+++ lib/excursion/pool/dummy_application.rb
@@ -7,6 +7,14 @@ module Excursion
         @routes.add(key.to_s, journey_route(key.to_s, Rails.application, journey_path("/#{name}/#{key.to_s}"), {required_defaults: []})) if @routes[key.to_sym].nil?
         @routes[key.to_sym]
       end
+
+      def [](key)
+        instance_variable_get("@#{key}".to_sym) || nil
+      end
+
+      def has_key?(key)
+        instance_variable_get("@#{key}".to_sym).nil?
+      end
     
     end
   end
