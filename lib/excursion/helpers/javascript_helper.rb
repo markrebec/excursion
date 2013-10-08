917aeb4cba4fdd41faa711fd268fad98adfdbeb9
Mark Rebec <mark@markrebec.com>
2013-10-08 14:29:45 -0700

    fixes typo'd reference

--- lib/excursion/helpers/javascript_helper.rb
+++ lib/excursion/helpers/javascript_helper.rb
@@ -2,7 +2,7 @@ module Excursion
   module Helpers
     module JavascriptHelper
       def render_excursion_javascript_helpers
-        content_tag :script, raw("Excursion.loadPool(#{raw Excursion::Pool.all_applications.map(&:to_cache).to_json});"), type: "text/javascript"
+        content_tag :script, raw("Excursion.loadPool(#{raw Excursion::Pool.all_applications.values.map(&:to_cache).to_json});"), type: "text/javascript"
       end
 
     end
