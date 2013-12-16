ee41851f92d47cffaca6145cdc383394aeedd235
Mark Rebec <mark@markrebec.com>
2013-12-16 13:31:03 -0800

    base64 encodes the raw route pool in the javascript helper

--- lib/excursion/helpers/javascript_helper.rb
+++ lib/excursion/helpers/javascript_helper.rb
@@ -2,7 +2,7 @@ module Excursion
   module Helpers
     module JavascriptHelper
       def render_excursion_javascript_helpers
-        content_tag :script, raw("Excursion.loadPool(#{raw Excursion::Pool.all_applications.values.map(&:to_cache).to_json});"), type: "text/javascript"
+        content_tag :script, raw("Excursion.loadPool('#{raw Base64.encode64(Excursion::Pool.all_applications.values.map(&:to_cache).to_json.to_s).gsub("\n","")}');"), type: "text/javascript"
       end
 
     end
