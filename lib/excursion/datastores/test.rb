f9e6001951579ee30b433b6279cac68c0213cad2
Mark Rebec <mark@markrebec.com>
2014-01-23 00:02:33 -0800

    refactors the test datastore a bit

--- lib/excursion/datastores/test.rb
+++ lib/excursion/datastores/test.rb
@@ -12,10 +12,10 @@ module Excursion
       end
       
       def read(key)
-        return @pool[key.to_sym] if key.to_s.match(/^_.*/) && @pool.has_key?(key.to_sym)
+        return @pool[key.to_sym] if option_key?(key)
 
-        return unless Excursion.configuration.test_providers.nil? || Excursion.configuration.test_providers.map(&:to_sym).include?(key.to_sym)
-        @pool[key.to_sym] ||= application_class.new(key, {default_url_options: {host: 'www.example.com'}}, ActionDispatch::Routing::RouteSet::NamedRouteCollection.new).to_cache
+        return unless test_provider?(key)
+        @pool[key.to_sym] ||= application_class.new(key, default_options, ActionDispatch::Routing::RouteSet::NamedRouteCollection.new).to_cache
       end
       alias_method :get, :read
       
@@ -38,6 +38,18 @@ module Excursion
       def initialize(pool=nil)
         @pool = {}
         @pool = pool.dup unless pool.nil?
+      end
+
+      def test_provider?(key)
+        Excursion.configuration.test_providers.nil? || Excursion.configuration.test_providers.map(&:to_sym).include?(key.to_sym)
+      end
+
+      def default_options
+        {default_url_options: {host: 'www.example.com'}}
+      end
+
+      def option_key?(key)
+        key.to_s.match(/^_.*/) && @pool.has_key?(key.to_sym)
       end
     end
   end
